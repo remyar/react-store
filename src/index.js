08a1e3fc97a0251be0ab239c9e3ebc94f517d7cf
Rémy <remy.arnaudin@sauermanngroup.com>
2024-12-30 15:38:14 +0100

    add

--- src/index.js
+++ src/index.js
@@ -1,74 +1,41 @@
+import React , { createContext , useReducer } from 'react';
 
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import Broadcast from './Broadcast';
-import withStoreProvider from './withStoreProvider';
+const StoreContext = createContext();
 
-class StoreProvider extends Component {
-    static propTypes = {
-        globalState: PropTypes.object.isRequired
-    }
-
-    constructor(props) {
-        super(props);
-        this.state = this.props.globalState;
-        this.persistConfig = { persist: false, key: "root", ...this.props.persistConfig };
-        if (this.persistConfig.persist && this.persistConfig.load) {
-            this.persistConfig.load();
-        }
-    }
-
-    setStateAsync = async (d) => {
-        return new Promise((resolve) => {
-            this.setState(d, () => {
-                resolve();
-            })
-        });
-    }
-
-    async __dispatch(updater, ...args) {
-        if (updater && updater.constructor && updater.call && updater.apply) {
-            let u = await updater(...args, async (d, ...a) => {
-                return await this.__dispatch(d, ...a);
-            }, () => {
-                return this.state;
-            }, this.props.extra);
-            await this.setStateAsync({ ...this.state, ...u });
-            if (this.persistConfig.persist && this.persistConfig.store) {
-                if (this.persistConfig.whitelist !== undefined) {
-                    let toSave = undefined;
-                    this.persistConfig.whitelist.forEach((key) => {
-                        if (u[key] != undefined) {
-                            if (toSave == undefined) {
-                                toSave = {};
-                            }
-                            toSave[key] = u[key];
-                        }
-                    });
-
-                    if (toSave != undefined) {
-                        this.persistConfig.store(toSave);
-                    }
-                }
-            }
-            return u;
-        }
-    }
-
-    createSetGlobalState = props => {
-        return this.__dispatch.bind(this);
-    }
-
-    render() {
-        return (
-            <Broadcast globalState={this.state} createSetGlobalState={this.createSetGlobalState}>
-                {this.props.children}
-            </Broadcast>
-        )
-    }
+const reducer = (oldState, newState) => {
+    return { ...oldState, ...newState };
 }
 
+function StoreProvider(props) {
+    return <StoreContext.Provider value={{ globalState: useReducer(reducer, { ...(props.globalState || {}) }) }} >
+        {props.children}
+    </StoreContext.Provider>;
+};
 
+function useStore() {
 
-export { StoreProvider, withStoreProvider }
+    const [globalState, dispatch] = React.useContext(StoreContext);
 
+    return [globalState, async (updater, ..._args) => {
+
+        let obj = undefined;
+
+        if (updater?.constructor?.name == 'Promise') {
+
+            async function _process() {
+                return new Promise((resolve, reject) => {
+                    updater.then(r => resolve(r)).catch(err => reject(err));
+                })
+            }
+
+            obj = await _process();
+        }
+
+        dispatch(obj == undefined ? updater : obj);
+
+        return obj == undefined ? updater : obj;
+
+    }]
+}
+
+export { StoreProvider, useStore };
